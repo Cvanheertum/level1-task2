c61c56213f8297d6e9f23ebdeb02cf63c0ebbf27
Connor van Heertum <connor.vanheertum@gmail.com>
2020-09-05 10:23:55 +0200

    Cleaned Code

--- MadLevel1Task2/app/src/main/java/com/example/madlevel1task2/MainActivity.kt
+++ MadLevel1Task2/app/src/main/java/com/example/madlevel1task2/MainActivity.kt
@@ -3,15 +3,13 @@ package com.example.madlevel1task2
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import com.example.madlevel1task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var ptAnswer1 = binding.ptAnswer1
-    private var ptAnswer2 = binding.ptAnswer2
-    private var ptAnswer3 = binding.ptAnswer3
-    private var ptAnswer4 = binding.ptAnswer4
+    private var resultaat = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,40 +28,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showResult() {
-        var result = 0
-        var answer1 = ptAnswer1.text.toString().capitalize()
-        var answer2 = ptAnswer2.text.toString().capitalize()
-        var answer3 = ptAnswer3.text.toString().capitalize()
-        var answer4 = ptAnswer4.text.toString().capitalize()
+        val ptAnswer1 = binding.ptAnswer1
+        val ptAnswer2 = binding.ptAnswer2
+        val ptAnswer3 = binding.ptAnswer3
+        val ptAnswer4 = binding.ptAnswer4
+        val answer1 = ptAnswer1.text.toString().capitalize()
+        val answer2 = ptAnswer2.text.toString().capitalize()
+        val answer3 = ptAnswer3.text.toString().capitalize()
+        val answer4 = ptAnswer4.text.toString().capitalize()
 
-        if (answer1 == "T") {
-            result++
-            ptAnswer1.setTextColor(Color.GREEN)
+        calculate(answer1, "T", ptAnswer1)
+        calculate(answer2, "F", ptAnswer2)
+        calculate(answer3, "F", ptAnswer3)
+        calculate(answer4, "F", ptAnswer4)
+
+        Toast.makeText(this, "You have $resultaat answers correct!",Toast.LENGTH_SHORT).show()
+    }
+
+    private fun calculate(result: String, answer: String, reference: TextView) {
+        if (answer == result) {
+            reference.setTextColor(Color.GREEN)
+            resultaat++
         } else {
-            ptAnswer1.setTextColor(Color.RED)
+            reference.setTextColor(Color.RED)
         }
-
-        if (answer2 == "F") {
-            result++
-            ptAnswer2.setTextColor(Color.GREEN)
-        } else {
-            ptAnswer2.setTextColor(Color.RED)
-        }
-
-        if (answer3 == "F") {
-            result++
-            ptAnswer3.setTextColor(Color.GREEN)
-        } else {
-            ptAnswer3.setTextColor(Color.RED)
-        }
-
-        if (answer4 == "F") {
-            result++
-            ptAnswer4.setTextColor(Color.GREEN)
-        } else {
-            ptAnswer4.setTextColor(Color.RED)
-        }
-
-        Toast.makeText(this, "You have $result answers correct!",Toast.LENGTH_SHORT).show()
     }
 }
